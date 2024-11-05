ff133e01696e08901963988fcfc82574aeb131ce
FAHRAOUI Zakaria <zakaria.fahraoui@gmail.com>
2024-11-05 14:07:04 +0100

    first commit

---
+++ src/main/java/com/planisense/treemanagement/TreeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.planisense.treemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planisense/treemanagement/TreeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planisense.treemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
